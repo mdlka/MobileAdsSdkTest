01df821623ed122c99c4ad2dc6db11e0549fba0c
mdlka <mogilevskiy499@gmail.com>
2023-04-18 11:38:55 +0300

    Add project

---
+++ Assets/Source/Scripts/Test.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private Button _playInterButton;
+    [SerializeField] private Button _playRewardButton;
+    [SerializeField] private Button _showBannerButton;
+
+    private YandexMobileAdsTest _yandexMobileAds;
+
+    private void Awake()
+    {
+        _yandexMobileAds = new YandexMobileAdsTest();
+    }
+
+    private void OnEnable()
+    {
+        _playInterButton.onClick.AddListener(OnPlayInterButton);
+        _playRewardButton.onClick.AddListener(OnPlayRewardButton);
+        _showBannerButton.onClick.AddListener(OnShowBannerButton);
+    }
+
+    private void OnDisable()
+    {
+        _playInterButton.onClick.RemoveListener(OnPlayInterButton);
+        _playRewardButton.onClick.RemoveListener(OnPlayRewardButton);
+        _showBannerButton.onClick.RemoveListener(OnShowBannerButton);
+    }
+
+    private void OnPlayRewardButton() => _yandexMobileAds.ShowRewardedAd();
+    private void OnShowBannerButton() => _yandexMobileAds.ShowBanner();
+    private void OnPlayInterButton() => _yandexMobileAds.ShowInterstitial();
+}
---
+++ Assets/Source/Scripts/YandexMobileAdsTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+using YandexMobileAds;
+using YandexMobileAds.Base;
+
+public class YandexMobileAdsTest
+{
+    private const string BannerId = "demo-banner-yandex";
+    private const string InterstitialId = "demo-interstitial-yandex";
+    private const string RewardedId = "demo-rewarded-yandex";
+    
+    private Banner _banner;
+    private Interstitial _interstitial;
+    private RewardedAd _rewardedAd;
+
+    private bool _interstitialLoading;
+    private bool _rewardedLoading;
+    
+    public void ShowBanner()
+    {
+        if (_banner != null)
+            return;
+        
+        MobileAds.SetAgeRestrictedUser(true); //Sets COPPA restriction for user age under 13
+
+        AdSize bannerMaxSize = AdSize.FlexibleSize(GetScreenWidthDp(), 100);
+        _banner = new Banner(BannerId, bannerMaxSize, AdPosition.BottomCenter);
+
+        _banner.OnAdLoaded += HandleBannerAdLoaded;
+        _banner.LoadAd(CreateAdRequest());
+    }
+    
+    public void ShowInterstitial()
+    {
+        if (_interstitialLoading)
+            return;
+        
+        MobileAds.SetAgeRestrictedUser(true); //Sets COPPA restriction for user age under 13
+
+        _interstitial ??= new Interstitial(InterstitialId);
+        
+        _interstitial.OnInterstitialLoaded += HandleInterstitialLoaded;
+        _interstitial.LoadAd(CreateAdRequest());
+        _interstitialLoading = true;
+    }
+    
+    public void ShowRewardedAd()
+    {
+        if (_rewardedLoading)
+            return;
+        
+        MobileAds.SetAgeRestrictedUser(true); //Sets COPPA restriction for user age under 13
+
+        _rewardedAd ??= new RewardedAd(RewardedId);
+
+        _rewardedAd.OnRewardedAdLoaded += HandleRewardedAdLoaded;
+        _rewardedAd.LoadAd(CreateAdRequest());
+        _rewardedLoading = true;
+    }
+
+    private void HandleBannerAdLoaded(object sender, EventArgs args)
+    {
+        _banner.Show();
+        _banner.OnAdLoaded -= HandleBannerAdLoaded;
+    }
+
+    private void HandleInterstitialLoaded(object sender, EventArgs args)
+    {
+        _interstitial.Show();
+        _interstitial.OnInterstitialLoaded -= HandleInterstitialLoaded;
+        _interstitialLoading = false;
+    }
+
+    private void HandleRewardedAdLoaded(object sender, EventArgs args)
+    {
+        _rewardedAd.Show();
+        _rewardedAd.OnRewardedAdLoaded -= HandleRewardedAdLoaded;
+        _rewardedLoading = false;
+    }
+
+    private int GetScreenWidthDp()
+    {
+        int screenWidth = (int)Screen.safeArea.width;
+        return ScreenUtils.ConvertPixelsToDp(screenWidth);
+    }
+    
+    private AdRequest CreateAdRequest() => new AdRequest.Builder().Build();
+}
